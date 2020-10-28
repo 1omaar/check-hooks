d147665309b973291cd1bde7e55e0329c7404a0b
omartebessi <chaweli10@gmail.com>
2020-10-28 13:01:03 +0100

    second version

--- src/App.js
+++ src/App.js
@@ -4,8 +4,30 @@ import MovieList from './Components/MovieComponent/MovieList'
 import AddMovie from './AddMovie';
 import Filter from './Components/Filter/Filter'
 const App = () => {
-  const [movie, setMovie]= useState([{image:"/images/the-100.jpg",title:"The 100",descrpt:"Une station spatiale abritant les derniers humains survivants après un incident nucléaire envoie 100 jeunes délinquants à la surface de la Terre, 97 ans plus tard, afin qu'ils la re-colonisent...",link:"https://www.zt-za.com/telecharger-serie/71705-telecharger-les-100-saison-7-hdtv-vostfr.htmlé",rate:3},
-      {image:"/images/the-blacklist.jpg",title:"The Blacklist",descrpt:"The Blacklist est une série télévisée américaine centrée sur un ancien agent nommé Raymond Red Reddington qui s'est échappé depuis des décennies et qui à présent est le plus recherché par le FBI. Soudainement, il se rend en personne au quartier général du FBI avec une offre sur l'emplacement d'un grand nombre de terroristes...",link:"http://www.cpasmieux.club/seriestreaming/drame-s/551-the-blacklist.html",rate:4}]) 
+  const [movie, setMovie]= useState([{
+      image:"/images/the-100.jpg",
+      title:"The 100",
+      descrpt:"Une station spatiale abritant les derniers humains survivants après un incident nucléaire envoie 100 jeunes délinquants à la surface de la Terre, 97 ans plus tard, afin qu'ils la re-colonisent...",
+      link:"https://www.zt-za.com/telecharger-serie/71705-telecharger-les-100-saison-7-hdtv-vostfr.htmlé",
+      rate:3},
+      {image:"/images/the-blacklist.jpg",
+      title:"The Blacklist",
+      descrpt:"The Blacklist est une série télévisée américaine centrée sur un ancien agent nommé Raymond Red Reddington qui s'est échappé depuis des décennies et qui à présent est le plus recherché par le FBI. Soudainement, il se rend en personne au quartier général du FBI avec une offre sur l'emplacement d'un grand nombre de terroristes...",
+      link:"http://www.cpasmieux.club/seriestreaming/drame-s/551-the-blacklist.html",
+      rate:4},
+      {image:"/images/see.jpg", 
+      title:'See', 
+      descrpt:"Dans un futur lointain, alors que la race humaine a perdu le sens de la vue, la société a dû trouver d'autres façons d'intéragir, de chasser, de construire et tout simplement de survivre. Cet équilibre est bousculé le jour où des jumeaux naissent avec la capacité de voir.",
+      link:"https://www.betaseries.com/serie/see",
+      rate:5},
+      {
+        image:"/images/the-last-kingdom.jpg",
+        title:"The Last Kingdom",
+        descrpt:"Histoire, politique et action sanguinaire se heurtent dans cette série britannique à la suite d'un Viking d'origine saxonne cherchant à revendiquer son droit d'aînesse ancestral et le royaume dirigé par Alfred le Grand alors qu'ils se défendent des envahisseurs nordiques.",
+        link:"https://www.flicks.co.nz/tv/the-last-kingdom-season-3/",
+        rate:2
+      } 
+    ]) 
       //create a function to update the new movie with the new value of component addMovie
       let updateMovies = (movies) =>{
       (movies.image === "") ? alert("Please Enter the url image") : (movies.title === "") ?
--- src/Components/MovieComponent/MovieList.js
+++ src/Components/MovieComponent/MovieList.js
@@ -2,12 +2,22 @@ import React from 'react'
 import MovieCard from './MovieCard'
 import './MovieList.css'
 const MovieList = ({myMovies, text, rates}) => {
+  if (text!=="") {
     return (
-        <div className='list_movie'>
-            {myMovies.filter(el => el.title.toLowerCase().includes(text.toLowerCase()) || el.rate == rates)
-            .map((el,i)=>(<MovieCard key={i} movie={el}/>))}
-        </div>
-    )
+      
+         
+            (<div className='list_movie'>
+                {myMovies.filter(el => el.title.toLowerCase().includes(text.toLowerCase()))
+                .map((el,i)=>(<MovieCard key={i} movie={el}/>))}
+            </div>)
+             );
+            }
+           
+       return   (<div className='list_movie'>
+                {myMovies.filter(el => el.rate>=rates)
+                .map((el,i)=>(<MovieCard key={i} movie={el}/>))}
+            </div>)
+            
 }
 
 export default MovieList
